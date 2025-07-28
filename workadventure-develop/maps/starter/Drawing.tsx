76df98f217c1d787b779b01f149a29c59c16e6ff
mirmammadkiani <01amirmkiani@gmail.com>
2025-07-28 19:27:07 +0330

    Add files via upload

---
+++ workadventure-develop/maps/starter/Drawing.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Drawing" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="../assets/Drawing.png" width="288" height="288"/>
+</tileset>
