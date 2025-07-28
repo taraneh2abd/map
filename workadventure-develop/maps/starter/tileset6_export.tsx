36cb129bc81400ee4694020b29bf623f8d5278e2
mirmammadkiani <01amirmkiani@gmail.com>
2025-07-28 20:41:08 +0330

    Add files via upload

---
+++ workadventure-develop/maps/starter/tileset6_export.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset6_export" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <properties>
+  <property name="tilesetCopyright">© 2021 WorkAdventure &lt;https://workadventu.re/&gt; 
+Licence: WORKADVENTURE SPECIFIC RESOURCES LICENSE (see LICENSE.assets file)</property>
+ </properties>
+ <image source="../assets/tileset6_export.png" width="320" height="320"/>
+</tileset>
