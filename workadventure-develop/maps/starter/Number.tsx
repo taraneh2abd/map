71e43a0cb13b01c07defa386c5ea2b10d8fc9fe5
mirmammadkiani <01amirmkiani@gmail.com>
2025-07-29 00:15:15 +0330

    Add files via upload

---
+++ workadventure-develop/maps/starter/Number.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Number" tilewidth="32" tileheight="32" tilecount="1849" columns="43">
+ <image source="../assets/Number.png" width="1400" height="1400"/>
+</tileset>
