452cfc06bbd085be6c17c3deb8330e86b68ea515
ImFardad <fardadgoodboy@gmail.com>
2025-07-28 19:12:52 +0330

    Delete workadventure-develop/maps directory

--- workadventure-develop/maps/starter/tileset1-repositioning.tsx
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset1-repositioning" tilewidth="32" tileheight="32" tilecount="121" columns="11">
- <properties>
-  <property name="tilesetCopyright">© 2021 WorkAdventure &lt;https://workadventu.re/&gt; 
-Licence: WORKADVENTURE SPECIFIC RESOURCES LICENSE (see LICENSE.assets file)</property>
- </properties>
- <image source="../assets/tileset1-repositioning.png" width="352" height="352"/>
-</tileset>
