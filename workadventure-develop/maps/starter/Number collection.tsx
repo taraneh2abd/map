a1e30eccd08676387837149d392308f8705e862b
alreadydonewithu <alreadydonewithu@gmail.com>
2025-07-28 23:58:44 +0330

    Delete workadventure-develop/maps directory

--- workadventure-develop/maps/starter/Number collection.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Number collection" tilewidth="32" tileheight="32" tilecount="1849" columns="43">
- <image source="../assets/Number collection.png" width="1400" height="1400"/>
-</tileset>
