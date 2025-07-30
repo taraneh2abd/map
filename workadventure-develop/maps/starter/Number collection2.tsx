0749026e19a7f0d2dd7c69a200a747bb0b3b044c
mirmammadkiani <01amirmkiani@gmail.com>
2025-07-30 16:06:50 +0330

    Add files via upload

---
+++ workadventure-develop/maps/starter/Number collection2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Number collection2" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../assets/Number collection2.png" width="280" height="280"/>
+</tileset>
