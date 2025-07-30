3778017e4fdfb3d590fd059fd4df0f18315d9da1
mirmammadkiani <01amirmkiani@gmail.com>
2025-07-30 17:09:19 +0330

    Delete workadventure-develop/maps directory

--- workadventure-develop/maps/starter/baby gil2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="baby gil2" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../assets/baby gil2.png" width="256" height="256"/>
-</tileset>
