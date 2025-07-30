33a770b0e4a30b90897b4dd4cbd612a333ed9be5
mirmammadkiani <01amirmkiani@gmail.com>
2025-07-30 16:10:59 +0330

    Delete workadventure-develop/maps directory

--- workadventure-develop/maps/starter/sampadLogo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="sampadLogo" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../assets/sampadLogo.png" width="256" height="256"/>
-</tileset>
