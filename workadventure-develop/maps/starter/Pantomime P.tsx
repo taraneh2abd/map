ed5ec859269d7864e71b67203e58995192666de2
mirmammadkiani <01amirmkiani@gmail.com>
2025-07-30 16:04:34 +0330

    Delete workadventure-develop/maps directory

--- workadventure-develop/maps/starter/Pantomime P.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Pantomime P" tilewidth="32" tileheight="32" tilecount="81" columns="9">
- <image source="../assets/Pantomime P.png" width="288" height="288"/>
-</tileset>
--- workadventure-develop/maps/starter/paternx.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="paternx" tilewidth="32" tileheight="32" tilecount="10000" columns="100">
- <image source="../assets/paternx.png" width="3200" height="3200"/>
-</tileset>
