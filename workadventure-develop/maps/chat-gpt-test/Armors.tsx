339458d400fc51046dfd4278b8df8a8f4ea72a8a
taraneh2abd <taraneh.abdellahi2@gmail.com>
2025-08-09 12:35:38 +0330

    add a simple exiturl

--- workadventure-develop/maps/chat-gpt-test/Armors.tsx
+++ workadventure-develop/maps/chat-gpt-test/Armors.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Chickens" tilewidth="32" tileheight="32" tilecount="16" columns="4">
   <image source="Special_Zones.png" width="64" height="32"/>
+  
+  <tile id="9">
+    <properties>
+      <property name="openWebsite" type="string" value="https://docs.google.com/document/d/1BD69UiNPbc-73PWEPbKAcPbELBjoPKDc1iMU7NoSme8/edit?tab=t.0"/>
+    </properties>
+  </tile>
+
 </tileset>
