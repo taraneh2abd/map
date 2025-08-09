3c9050eb622f9af6d847f7a0c1dc45e2fa00430d
taraneh2abd <taraneh.abdellahi2@gmail.com>
2025-08-09 12:05:02 +0330

    add zones

--- workadventure-develop/maps/chat-gpt-test/Armors.tsx
+++ workadventure-develop/maps/chat-gpt-test/Armors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Armors" tilewidth="25" tileheight="25" tilecount="90" columns="10">
-  <image source="Armors.png" width="250" height="250"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Chickens" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+  <image source="Special_Zones.png" width="64" height="32"/>
 </tileset>
