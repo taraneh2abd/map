afc3222d9659d34fd677de3a7c3a315cd68bcf78
taraneh2abd <taraneh.abdellahi2@gmail.com>
2025-08-09 11:54:07 +0330

    add  chickens

---
+++ workadventure-develop/maps/chat-gpt-test/Armors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Armors" tilewidth="25" tileheight="25" tilecount="90" columns="10">
+  <image source="Armors.png" width="250" height="250"/>
+</tileset>
---
+++ workadventure-develop/maps/chat-gpt-test/Chickens.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Chickens" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+  <image source="Chickens.png" width="64" height="32"/>
+</tileset>
